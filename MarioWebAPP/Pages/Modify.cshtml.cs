1f0c9579451bba9bbf4cfc856aea7dfaa3d4a1df
SEASONIC\mariolin <mariolin@seasonic.com.tw>
2023-05-02 15:43:06 +0800

    Search to Modify on process

--- MarioWebAPP/Pages/Modify.cshtml.cs
+++ MarioWebAPP/Pages/Modify.cshtml.cs
@@ -1,12 +1,43 @@
+using MarioWebAPP.Datas;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+using System.Linq;
+using System.Collections.Generic;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace MarioWebAPP.Pages
 {
     public class ModifyModel : PageModel
     {
+        private readonly IConfiguration Configuration;
+        public ModifyModel(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPostGetMember(IFormCollection formcollection)
+        {
+            var memberNo = formcollection["memberNo"].ToString();
+
+            var conn = new DapperConnections.ConnectionOptions();
+            Configuration.GetSection(DapperConnections.ConnectionOptions.Position).Bind(conn);
+            var sql = @"select * from UserInfo where MemberNo =  @membernumber";
+            using (var con = new SqlConnection(conn.RookieServerContext))
+            {
+                var result = con.Query(sql, new
+                {
+                    membernumber=memberNo
+                }).ToList();
+                return new JsonResult(result);
+            }
+
         }
     }
 }
